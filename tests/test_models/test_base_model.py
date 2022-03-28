16446003496fc8d03716ce2694edc89c1e6b1ae0
MarionBaussart <marion.baussart@gmail.com>
2022-03-28 12:50:52 +0000

    unitest + __init__

--- tests/test_models/test_base_model.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from models.base_model import BaseModel
-
-class TestBaseModel(unittest.TestCase):
-
-    def test_print(self):
-        printable = BaseModel.__str__
-        assert printable is not None
